34f99d05a4a0d5694fc9e029b49be0e6cb4fec1c
Valery Bogomolov <texter.coretext@gmail.com>
2021-12-09 02:36:12 +0200

    reset log and temp files before run with the help of 'new' argument

--- run.php
+++ run.php
@@ -13,6 +13,14 @@ require_once('lib/functions.php');
 require_once('lib/phpQuery.php');
 
 
-// parser to run
-// require_once('parsers/wp_parse_docs.php');
+if (isset($argv[1]) && $argv[1] === 'new') {
+    newParse();
+}
+
+#
+# parser to run
+#
+
 require_once('parsers/php_parse_docs.php');
+// require_once('parsers/wp_parse_docs.php');
+// require_once('misc/wp_add_first_slide.php');
